717a773d0455591e52d798bfad3159355a49f500
snayder29 <49761104+snayder29@users.noreply.github.com>
2019-10-02 00:10:22 -0500

    conection bd

---
+++ src/Conexion/conection.java
@@ -0,0 +1,17 @@
+package Conexion;
+
+import java.sql.*;
+
+public class conection {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException
+	{
+	
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String connectionURL = "jdbc:sqlserver://snayderbryan.database.windows.net:1433;databaseName=Ruta_21;user=snayder@snayderbryan;password=Espartan117;encrypt=true;trustServerCertificate=false;loginTimeout=30";
+			Connection con = DriverManager.getConnection(connectionURL);
+			
+			System.out.println("nos conectamos");
+	}		
+
+}
